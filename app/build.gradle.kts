efca365f8f3a5342f5f43d165b1d6195ce01456b
Zuher <nightmonkey223@gmail.com>
2026-08-31 20:58:13 +0700

    Bump version to 1.0.3 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
 
     defaultConfig {
         applicationId = "com.zdon.app"
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
